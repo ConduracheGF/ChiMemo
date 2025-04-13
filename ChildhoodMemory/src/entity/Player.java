58134c476df6fa2633d2067dad1414b39bd377a5
ConduracheGF <condurachegabriel14@gmail.com>
2025-04-13 20:14:51 +0300

    am reusit sa facem o proba de harta pentru nivelul 1 pentru nivelul 1 si sa implementam camera

--- ChildhoodMemory/src/entity/Player.java
+++ ChildhoodMemory/src/entity/Player.java
@@ -12,17 +12,24 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH)
     {
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
+
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues(){
-        x = 100;
-        y = 100;
-        speed = 2;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
+        speed = 5;
         direction = "down";
     }
 
@@ -75,16 +82,16 @@ public class Player extends Entity{
         {
             if (keyH.upPressed == true) {
                 direction = "up";
-                y  -= speed;
+                worldY  -= speed;
             } else if(keyH.downPressed == true) {
                 direction = "down";
-                y  += speed;
+                worldY  += speed;
             } else if (keyH.leftPressed == true) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             } else if (keyH.rightPressed == true) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
             spriteCounter++;
             if(spriteCounter > 12)
@@ -288,6 +295,6 @@ public class Player extends Entity{
                 }
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
--- ChildhoodMemory/src/main/GamePanel.java
+++ ChildhoodMemory/src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 
@@ -11,22 +12,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int maxScreenCol = 15;
+    public final int maxScreenRow = 15;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // WORLD SETTINGS
+    public final int maxWorldCol = 40;
+    public final int maxWorldRow = 30;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
     //FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this,keyH);
-
-    //Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 2;
+    public Player player = new Player(this,keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -104,7 +106,11 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
+
+        tileM.draw(g2);
+
         player.draw(g2);
+
         g2.dispose();
     }
 }
